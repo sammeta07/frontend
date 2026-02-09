44d24e4545cbcb851251e835a7d591ed3acf276a
S Hari Prasad <91180821+sammeta07@users.noreply.github.com>
2026-02-09 06:17:25 +0000

    restructure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'home', component: HomeComponent }
 ];
