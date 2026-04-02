f97bab103b97f95ac81ae432f52f5bcac74b06a0
S Hari Prasad <91180821+sammeta07@users.noreply.github.com>
2026-04-02 05:15:49 +0000

    public component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { PublicComponent } from './features/public/public.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent }
+  { path: '', component: PublicComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'public', component: PublicComponent },
+
 ];
---
+++ src/app/features/public/public.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-public',
+  imports: [],
+  templateUrl: './public.component.html',
+  styleUrl: './public.component.css',
+})
+export class PublicComponent {
+
+}
