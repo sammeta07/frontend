96d7f11505de989a437b38f576f83137c0357b6f
S Hari Prasad <91180821+sammeta07@users.noreply.github.com>
2026-04-08 10:49:44 +0000

    remove PublicComponent and redirect default route to Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
-import { PublicComponent } from './features/public/public.component';
 
 export const routes: Routes = [
-  { path: '', component: PublicComponent },
+  { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'public', component: PublicComponent },
-
 ];
